ce4b6ee14eb817ae9bbb4c7c80b82f09cac4e521
Abdullah <shabdullah685@gmail.com>
2022-05-11 14:48:07 +0530

    finish up image upload and thumbnail virtual

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/yelp-camping/image/upload/w_300/v1652252610/yelp-camp/htv9eth2v9dxucidxwob.jpg
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
 const CampgroundSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
